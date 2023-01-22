57e22a07b6dd614de697a0d04a0a203027248911
wjuneo-f <warleyjuneo111@gmail.com>
2023-01-22 12:19:50 -0300

    rafactor: ex00 refactor

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -8,10 +8,10 @@ template <typename T>
 void easyfind(T destiny, int source) {
     typename T::iterator iter = std::find(destiny.begin(), destiny.end(), source);
     if (iter != destiny.end() && source == *(destiny.end() - 1)) {
-        std::cout << "Value find" << std::endl;
+        std::cout << "value found in container" << std::endl;
     }
     else {
-         throw std::runtime_error("Integer not found inside container");
+         throw std::runtime_error("Integer not found in container");
     }
    
 }
